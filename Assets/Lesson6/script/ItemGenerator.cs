fec96657d063b3f208ce687891c21ac571c258ed
TsukasaOnose <70473575+TsukasaOnose@users.noreply.github.com>
2020-09-21 23:33:29 +0900

    Lesson6　課題修正版 途中

--- Assets/Lesson6/script/ItemGenerator.cs
+++ Assets/Lesson6/script/ItemGenerator.cs
@@ -11,44 +11,56 @@ public class ItemGenerator : MonoBehaviour
     //conePrefabを入れる
     public GameObject conePrefab;
     //スタート地点
-    private float startPos = 80.0f;
+    private float startPos = 30.0f;
     //ゴール地点
-    private float goalPos = 360.0f;
+    private float goalPos = 300.0f;
     //アイテムを出すx方向の範囲
     private float posRange = 3.4f;
     //アイテムを生成する位置（プレイヤーからの距離）
     private float itemGenerateDistance = 50.0f;
-    //最後に生成したアイテムの座標
-    public Vector3 lastItem;
+    //unityちゃんの進んだ距離を計算する為の基準点
+    private Vector3 basePos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //アイテム生成の基準の座標
-        lastItem = new Vector3(0, 0, 0);
+        //unityちゃんの初期地点の座標を基準点として取得
+        basePos = GameObject.Find("unitychan").transform.position;
     }
+
 
     // Update is called once per frame
     void Update()
     {
         //Unityちゃんの現在の座標を取得
         Vector3 nowPos = GameObject.Find("unitychan").transform.position;
-        //現在地点と最後に生成したアイテムの距離
-        float distance = lastItem.z - nowPos.z;
+        //現在地点と初期地点との距離
+        float distance = nowPos.z - basePos.z;
 
-        //生成したアイテムとunityちゃんとの距離が35m以内且つ、ゴールしてない時
-        if (distance <= 35.0f && nowPos.z <= goalPos - itemGenerateDistance)
+
+        //unityちゃんが15m進む毎にアイテムを生成する。
+
+        //距離が15m以上になった時
+        if (distance >= 15.0f　&&　nowPos.z > startPos && nowPos.z < goalPos)
         {
             //どのアイテムを出すのかをランダムに設定
             int num = Random.Range(1, 11);
             if (num <= 2)
             {
+                //アイテムを置くZ座標のオフセットをランダムに設定
+                int offsetZ = Random.Range(-10, 1);
                 //コーンをx軸方向に一直線に生成
                 for (float j = -1f; j <= 1f; j += 0.4f)
                 {
                     GameObject cone = Instantiate(conePrefab);
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, nowPos.z + itemGenerateDistance);
-                    lastItem = cone.transform.position;
+                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, nowPos.z + itemGenerateDistance + offsetZ);
+                    //現在地点を基準点として更新する
+                    basePos = nowPos;
+                    if (cone.transform.position.z < nowPos.z)
+                    {
+                        GameObject.Destroy(cone);
+                    }
                 }
             }
             else
@@ -56,24 +68,30 @@ public class ItemGenerator : MonoBehaviour
                 //レーンごとにアイテムを生成
                 for (int j = -1; j <= 1; j++)
                 {
-                    //アイテムの種類を決める
-                    int item = Random.Range(1, 11);
                     //アイテムを置くZ座標のオフセットをランダムに設定
                     int offsetZ = Random.Range(-10, 1);
+                    //アイテムの種類を決める
+                    int item = Random.Range(1, 11);
                     //60%コイン配置:30%車配置:10%何もなし
                     if (1 <= item && item <= 6)
                     {
                         //コインを生成
                         GameObject coin = Instantiate(coinPrefab);
                         coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, nowPos.z + itemGenerateDistance + offsetZ);
-                        lastItem = coin.transform.position;
+                        //現在地点を基準点として更新する
+                        basePos = nowPos;
+                        if(coin.transform.position.z < nowPos.z)
+                        {
+                            GameObject.Destroy(coin);
+                        }
                     }
                     else if (7 <= item && item <= 9)
                     {
                         //車を生成
                         GameObject car = Instantiate(carPrefab);
                         car.transform.position = new Vector3(posRange * j, car.transform.position.y, nowPos.z + itemGenerateDistance + offsetZ);
-                        lastItem = car.transform.position;
+                        //現在地点を基準点として更新する
+                        basePos = nowPos;
                     }
                 }
             }
